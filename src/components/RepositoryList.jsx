eeb358af1c3ea2c79d455d21976f4038f4b4e572
Ana Ventura <anaventura1811@gmail.com>
2021-05-05 11:41:14 -0300

    Sobre useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -15,6 +15,12 @@ const repository = {
 export default function RepositoryList() {
   const [repositories, setRepositories] = useState([]) // sempre que é uma lista, começar o vetor do estado com array vazio
   
+  useEffect(() => {
+
+  }, [repositories]);   // recebe dois parâmetros: 1)função e 2)quando executar/dependências -- info que, quando mudarem, useEffect tem que executar
+
+ // se eu passo um array vazio, essa funcao só executa uma única vez
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
