1dc9a0ca054ce5d44e664c9c7a011f492cab67cc
Ana Ventura <anaventura1811@gmail.com>
2021-05-04 21:33:19 -0300

    Informações sobre imutabilidade

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export default function Counter() {
-  const [counter, setCounter] = useState(0); // Primeiro importar o hook e desestruturar em variavel e funcao
+  const [counter, setCounter] = useState(0); // Primeiro importar o hook e desestruturar em variavel e função
 
   function increment() {
     setCounter(counter + 1); // depois chamar a função definida com useState pra alterar o estado
@@ -14,3 +14,9 @@ export default function Counter() {
     </div>
   )
 }
+
+// imutabilidade - variável nunca vai ter seu valor alterado, não pode alterar diretamente o conteúdo 
+// sobre inclusão de itens em arrays - não usar o push
+// imutabilidade - conceito de estado do React
+// para alterar array é sempre preciso criar um novo array, usar spread operator e adicionar novo
+// item ao final
